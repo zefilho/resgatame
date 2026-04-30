939ae7ab5276e58817656b0e9f9b767ac7fcf31b
Ze <jose.lopes@itaipuparquetec.org.br>
2026-04-30 17:02:10 -0300

    chore: update next.js to fix CVE-2025-55182

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -17,15 +14,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  experimental: {
-    // This is required to allow the Next.js dev server to accept requests from
-    // the Firebase Studio development environment.
-    allowedDevOrigins: [
-        '6000-firebase-studio-1749052338704.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev',
-        '9000-firebase-studio-1749052338704.cluster-4xpux6pqdzhrktbhjf2cumyqtg.cloudworkstations.dev',
-    ],
-  },
+  }
 };
 
 export default nextConfig;
